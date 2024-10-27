fba18b4f7f732cd9129f264fbfb143af5632c8d6
George Taylor <drumerboygt.gt@gmail.com>
2024-10-27 02:50:27 -0400

    Final Project 3

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,17 +7,28 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=headers)
-    
     json_response = json.loads(response.text)
-    emotions = json_response['emotionPredictions'][0]['emotion']
-    
-    score = 0
-    
-    for emotion in emotions:
-        if(score < emotions[emotion]):
-            score = emotions[emotion]
-            highest = {'dominant_emotion': emotion}
 
-    emotions.update(highest)
-    
+    emotions = {}
+
+    if response.status_code == 200:
+        
+        emotions = json_response['emotionPredictions'][0]['emotion']  
+        
+        score = 0
+        
+        for emotion in emotions:
+            if(score < emotions[emotion]):
+                score = emotions[emotion]
+                highest = {'dominant_emotion': emotion}
+
+        emotions.update(highest) 
+
+    elif response.status_code == 400 or response.status_code == 500:
+
+        for emotion in emotions:
+            emotions[emotion] = None
+
+        emotions.update({'dominant_emotion': None}) 
+
     return emotions
--- server.py
+++ server.py
@@ -1,3 +1,29 @@
+""" Server for the App """
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def mood_detector():
+
+    """ Emotion Analyzer, linked to emotion_detection.py file """
+    user_text = request.args.get('textToAnalyze')
+
+    resp = emotion_detector(user_text)
+
+    if resp['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+
+    first_return = f"For the given statement, the system response is {resp}. "
+    second_return = f"The dominant emotion is {resp['dominant_emotion']}."
+    return first_return + second_return
+
+@app.route("/")
+def render_home_page():
+
+    """ Homepage render """
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
